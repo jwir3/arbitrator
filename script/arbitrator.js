f45ffa4f9682aeab95593c14af8aef48e946886e
Scott Johnson <jaywir3@gmail.com>
2014-05-13 20:21:02 -0500

    Modify arbitrator to be able to handle multi-line input from ArbiterSports via cut and paste.

--- script/arbitrator.js
+++ script/arbitrator.js
@@ -1,30 +1,37 @@
 var Arbitrator = function(aString) {
   this.mBaseString = aString;
-  this.parse();
+  this.parseFromText();
 }
 
 Arbitrator.prototype = {
-  parse: function() {
-    this.mRows = this.mBaseString.split('\n');
-    console.log("***** DEBUG_jwir3: Base String: " + this.mBaseString);
-    for (row in this.mRows) {
-      console.log("***** DEBUG_jwir3: " + this.mRows[row]);
+  parseFromText: function() {
+    this.mBaseString = this.mBaseString.replace(/Accepted\ on\ [0-9]+\/[0-9]+\/([0-9]{4})/g, '')
+    var cols = this.mBaseString.split(/[\t\n]+/);
+    this.mTable = new Array();
+    var row = new Array();
+    var i = 0;
+    var removedLastCol= false;
+    for (col in cols) {
+      var trimmedCol = cols[col].trim();
+      console.log("***** DEBUG_jwir3: Trimmed column is: " + trimmedCol);
+      if (trimmedCol.length != 0) {
+        row.push(trimmedCol);
+      } else if (i == 1){
+        row.push("NONE");
+      }
+
+      i = i + 1;
+      if (i%10 == 0) {
+        this.mTable.push(row);
+        row = new Array();
+      }
     }
 
-    this.mTable = new Array(this.mRows.length);
-    var i = 0;
-    for (row in this.mRows) {
-      var cols = this.mRows[row].split("\t");
-      for (colNum in cols) {
-        cols[colNum] = cols[colNum].trim();
-      }
-      this.mTable[i] = cols;
-      i++;
-    }
+    this.mTable.push(row);
   },
 
   getRows: function() {
-    return this.mRows;
+    return this.mTable;
   },
 
   getColumns: function(aRow) {
@@ -33,7 +40,7 @@ Arbitrator.prototype = {
 
   getDateString: function(aRow) {
     // The date/time of the game will always be in column 4
-    return this.mTable[aRow][4];
+    return this.mTable[aRow][3];
   },
 
   getDate: function(aRow) {
@@ -51,7 +58,8 @@ Arbitrator.prototype = {
    */
   getRole: function(aRow) {
     // Role is column 3, and can be either "Referee" or "Linesman".
-    var roleString = this.mTable[aRow][3];
+    var roleString = this.mTable[aRow][2];
+    console.log("***** DEBUG_jwir3: roleString: " + roleString);
     if (roleString.search(/referee/i) != -1) {
       return 0;
     } else if (roleString.search(/linesman/i) != -1) {
--- script/test/arbitrator-tests.js
+++ script/test/arbitrator-tests.js
@@ -1,7 +1,6 @@
-/*
 test("Arbitrator Parse Test", function() {
   // Arrange
-  var testString = "4001 			Referee 2 	5/9/2014 Fri 5:40 PM 	OSL, Preview Challenge Cup Advance Mite 	Minnesota Made 1 	Adv Mite SE 	Adv Mite NW 	$39.00 Accepted on 3/31/2014";
+  var testString = "1111 		106016 	Referee 1 	11/9/2013 Sat 12:30 PM 	D6, 12B 	Bloomington Ice Garden 1 	Bloomington 	Minnetonka Black 	$29.50  Accepted on 10/18/2013";
 
   // Act
   var arbitrator = new Arbitrator(testString);
@@ -10,16 +9,15 @@ test("Arbitrator Parse Test", function() {
   // Assert
   ok(arbitrator != null, "arbitrator should be non-null");
   ok(arbitrator.getRows().length == 1, "there should be exactly one row");
-  ok(cols.length == 10, "row 0 should have 10 columns. It has: " + cols.length + " columns");
-  ok (cols[0] == '4001', "Column 0 should be the game number 4001. It was: " + cols[0]);
-  ok (cols[3] == 'Referee 2', "Column 3 should indicate referee 2. It was: " + cols[3]);
+  ok(cols.length == 9, "row 0 should have 9 columns. It has: " + cols.length + " columns");
+  ok (cols[0] == '1111', "Column 0 should be the game number 1111. It was: " + cols[0]);
+  ok (cols[2] == 'Referee 1', "Column 3 should indicate referee 1. It was: " + cols[3]);
 });
-*/
-/*
+
 test("Arbitrator Date Recognition", function() {
   // Arrange
-  var testString = "4001 			Referee 2 	5/9/2014 Fri 5:40 PM 	OSL, Preview Challenge Cup Advance Mite 	Minnesota Made 1 	Adv Mite SE 	Adv Mite NW 	$39.00 Accepted on 3/31/2014";
-  var arbitrator = new Arbitrator(testString2);
+  var testString = "1111 		106016 	Referee 1 	11/9/2013 Sat 12:30 PM 	D6, 12B 	Bloomington Ice Garden 1 	Bloomington 	Minnetonka Black 	$29.50  Accepted on 10/18/2013";
+  var arbitrator = new Arbitrator(testString);
 
   // Act
   var date = arbitrator.getDate(0);
@@ -27,18 +25,17 @@ test("Arbitrator Date Recognition", function() {
 
   // Assert
   ok(date.getDate() == '9', "Day of month should be 9. Was: " + date.getDate());
-  ok(date.getMonth() == '4', "Month should be 4. Was: " + date.getMonth());
-  ok(date.getFullYear() == '2014', "Year should be 2014. Was: " + date.getFullYear());
-  ok(date.getHours() == 17, "Game is at hour 5pm");
-  ok(date.getMinutes() == 40, "Game is at minute 40 of hour");
+  ok(date.getMonth() == '10', "Month should be 10. Was: " + date.getMonth());
+  ok(date.getFullYear() == '2013', "Year should be 2013. Was: " + date.getFullYear());
+  ok(date.getHours() == 12, "Game is at hour 12pm");
+  ok(date.getMinutes() == 30, "Game is at minute 30 of hour");
 });
-*/
 
 test("Arbitrator Role Recognition", function() {
   // Arrange
-  var testString = "4001 			Referee 2 	5/9/2014 Fri 5:40 PM 	OSL, Preview Challenge Cup Advance Mite 	Minnesota Made 1 	Adv Mite SE 	Adv Mite NW 	$39.00 Accepted on 3/31/2014";
+  var testRef = "1111 		106016 	Referee 1 	11/9/2013 Sat 12:30 PM 	D6, 12B 	Bloomington Ice Garden 1 	Bloomington 	Minnetonka Black 	$29.50  Accepted on 10/18/2013";
   var linesString = "598 		Showcase 	Linesman 	4/26/2014 Sat 8:15 PM 	Hockey, 16U AAA 	Saint Louis Park, East 	TBA 	TBA 	$38.00";
-  var arbitrator = new Arbitrator(testString);
+  var arbitrator = new Arbitrator(testRef);
   var arbitrator2 = new Arbitrator(linesString);
 
   // Act
