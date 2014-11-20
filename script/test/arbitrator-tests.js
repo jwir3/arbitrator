3f1d9f9a5cf9ca3e104e318dcb5e84b6d5a37c5b
Scott Johnson <jaywir3@gmail.com>
2014-11-19 20:38:16 -0600

    Issue #15: Add a test to verify that games differing only in date/time do not have different hashes.

--- script/test/arbitrator-tests.js
+++ script/test/arbitrator-tests.js
@@ -313,6 +313,7 @@ test("Tournament and Scrimmage Parsing", function() {
 
 test("Identification String and Hash Computation", function() {
   var testData = "1111 		106016 	Referee 1 	11/9/2013 Sat 12:30 PM 	D6, 12B 	Bloomington Ice Garden 1 	Bloomington 	Minnetonka Black 	$29.50  Accepted on 10/18/2013";
+
   var arbitrator = new Arbitrator(testData);
   var game = arbitrator.getGameById(1111);
   ok(game, "game should not be undefined");
@@ -321,4 +322,12 @@ test("Identification String and Hash Computation", function() {
   var expectedHash = CryptoJS.SHA1(expectedIdString).toString(CryptoJS.enc.Hex);
   equal(game.getIdentificationString(), expectedIdString, "identification string should be " + expectedIdString);
   equal(game.getHash(), expectedHash, "hash should be " + expectedHash);
+
+  // Now, test to make sure changing the date doesn't actually change the hash.
+  var testDataNewDate = "1111 		106016 	Referee 1 	12/15/2013 Sun 12:30 PM 	D6, 12B 	Bloomington Ice Garden 1 	Bloomington 	Minnetonka Black 	$29.50  Accepted on 10/18/2013";
+  var arbitratorNewDate = new Arbitrator(testDataNewDate);
+  var gameNewDate = arbitrator.getGameById(1111);
+
+  ok(gameNewDate, "game with a new date but all other information intact should not be undefined");
+  equal(game.getHash(), gameNewDate.getHash(), "two games differing only in date/time should have the same hash");
 });
