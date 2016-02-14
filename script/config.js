6e99025ef16bdd7183642c9b9d6fb117b196e6ce
Scott Johnson <jaywir3@gmail.com>
2016-02-14 10:48:08 -0600

    (#37): Add replace module to grunt so we can replace google keys in source.

--- script/config.js
+++ script/config.js
@@ -3,6 +3,6 @@ module.exports = {
    * Set this value to the Google Web Application Client ID you generated when
    * creating a new OAuth2 permission in your Google Developer Console.
    */
-  'google_client_id': '609265890429-c5d69skkd9o2f1fksco00294oom2burf.apps.googleusercontent.com',
-  'google_api_key': 'AIzaSyDkiyjtUnxlzQUyxnu0WqUtkhyV8Q2KyA0'
+  'google_client_id': '@@googleClientId',
+  'google_api_key': '@@googleAPIKey'
 };
