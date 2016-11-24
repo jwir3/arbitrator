d9d46deafe5499c116e7c96f489b4e4a54594e59
Scott Johnson <jaywir3@gmail.com>
2016-11-24 11:42:27 -0600

    Fix incorrectly capitalized filename causing string resolution problems.
    'strings.json' was incorrectly spelled as 'Strings.json' in the Strings.js
    file. This is fine on non-case-sensitive file systems, such as those in Mac
    OS/X and Windows, but is not conducive to correct file path resolution on
    linux or other case-sensitive file systems.
    
    Fixes #79.

--- src/arbitrator/Strings.js
+++ src/arbitrator/Strings.js
@@ -1,3 +1,3 @@
 import jetpack from 'fs-jetpack'; // module loaded from npm
 
-export var Strings = jetpack.cwd(__dirname).read('Strings.json', 'json');
+export var Strings = jetpack.cwd(__dirname).read('strings.json', 'json');
